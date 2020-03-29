fe8bf06da2e5a7e41d052857f667a4a98c522221
Mathews Alufwani <mathewalufwani@gmail.com>
2020-03-30 02:26:07 +0300

    add functionality to the century of birth field

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,22 @@
 function nameReveal(DD, MM, YY, CC, GN) {
 
     var DD = parseInt(document.getElementById("DD").value);
-    if (DD < 1 || DD > 31 || DD !==DD){
+    if (DD < 1 || DD > 31 || DD !== DD){
         alert("Please enter a valid day of birth")
     }
 
     var MM = parseInt(document.getElementById("MM").value);
-    if (MM < 1 || MM > 12 || MM !==MM){
+    if (MM < 1 || MM > 12 || MM !== MM){
         alert("Please enter a valid month of birth")
     }
 
     var YY = parseInt(document.getElementById("YY").value);
-    if (YY < 1 || YY > 99 || YY!==YY){
+    if (YY < 1 || YY > 99 || YY !== YY){
         alert("Please enter a valid year of birth")
+    }
+
+    var CC = parseInt(document.getElementById("CC").value);
+    if (CC < 1 || CC > 20 || CC !== CC){
+        alert("Please enter a valid century of birth")
     }
 }
