cf52214ac4bc06e204767d2ed1f232b93c39fb1e
Mathews Alufwani <mathewalufwani@gmail.com>
2020-03-30 01:53:03 +0300

    add functionality to day of birth field

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+function nameReveal(DD, MM, YY, CC, GN) {
+
+    var DD = parseInt(document.getElementById("DD").value);
+    if (DD < 1 || DD > 31 || DD !==DD){
+        alert("Please enter a valid day of birth")
+    }
+
+
+}
