fd748ce036a4653f006c4cb3e10734338121fa3a
Mathews Alufwani <mathewalufwani@gmail.com>
2020-03-30 02:13:29 +0300

    add functionality to year of birth field

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,12 @@ function nameReveal(DD, MM, YY, CC, GN) {
     }
 
     var MM = parseInt(document.getElementById("MM").value);
-    if (MM < 1 || MM > 12 || MM !==DD){
+    if (MM < 1 || MM > 12 || MM !==MM){
         alert("Please enter a valid month of birth")
+    }
+
+    var YY = parseInt(document.getElementById("YY").value);
+    if (YY < 1 || YY > 99 || YY!==YY){
+        alert("Please enter a valid year of birth")
     }
 }
